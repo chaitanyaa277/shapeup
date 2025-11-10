c9be5ab9aef148402216ceac0a072bd49b6b0f21
chaitanya <chaitanyachaitu27282006@gmail.com>
2025-11-10 22:51:45 +0530

    Update errorMiddleware.js

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,22 +1,17 @@
+// backend/middleware/errorMiddleware.js
 const notFound = (req, res, next) => {
-    const error = new Error(`Not Found - ${req.originalUrl}`);
-    res.status(404);
-    next(error);
-  };
-  
-  const errorHandler = (err, req, res, next) => {
-    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    let message = err.message;
-  
-    if (err.name === "CastError" && err.kind === "ObjectId") {
-      statusCode = 404;
-      message = "Resource not found";
-    }
-  
-    res.status(statusCode).json({
-      message,
-      stack: process.env.NODE_ENV === "production" ? null : err.stack,
-    });
-  };
-  
-  export { notFound, errorHandler };
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode && res.statusCode !== 200 ? res.statusCode : 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
+
+module.exports = { notFound, errorHandler };
