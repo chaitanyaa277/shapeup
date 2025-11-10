ae4d9e136e04e59d6c841e27ea3cb5e3c0a32828
chaitanya <chaitanyachaitu27282006@gmail.com>
2025-11-10 22:52:32 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,14 @@
-import mongoose from "mongoose";
+// backend/config/db.js
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.meessage}`);
+    console.error(`❌ MongoDB Error: ${error.message}`);
     process.exit(1);
   }
 };
 
-export default connectDB;
+module.exports = connectDB;
